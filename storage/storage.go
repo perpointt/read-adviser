b5944fe7f0b384061d3c4e5fa632b1c537ae5bcf
Maxim Ilyshchenko <per.pointt@gmail.com>
2024-10-10 13:41:59 +0300

    feat: add interfaces && files storage

---
+++ storage/storage.go
@@ -0,0 +1,36 @@
+package storage
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"read-adviser/lib/e"
+)
+
+type Storage interface {
+	Save(p *Page) error
+	PickRandom(userName string) (*Page, error)
+	Remove(p *Page) error
+	IsExists(p *Page) (bool, error)
+}
+
+type Page struct {
+	URL      string
+	UserName string
+}
+
+func (p *Page) Hash() (hash string, err error) {
+	defer func() { err = e.WrapIfErr("can't calculate hash", err) }()
+
+	h := sha1.New()
+
+	if _, err := io.WriteString(h, p.URL); err != nil {
+		return "", err
+	}
+
+	if _, err := io.WriteString(h, p.UserName); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
