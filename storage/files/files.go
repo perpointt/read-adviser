7e507f3c3f1b62b427221c8b2a894a689a2494c5
Maxim Ilyshchenko <per.pointt@gmail.com>
2024-10-10 19:17:09 +0300

    feat: add events and updates

--- storage/files/files.go
+++ storage/files/files.go
@@ -18,8 +18,6 @@ type Storage struct {
 
 const defaultPerm = 0774
 
-var ErrNoSavedPages = errors.New("no saved page")
-
 func New(basePath string) Storage {
 	return Storage{basePath: basePath}
 }
@@ -65,7 +63,7 @@ func (s Storage) PickRandom(userName string) (page *storage.Page, err error) {
 	}
 
 	if len(files) == 0 {
-		return nil, ErrNoSavedPages
+		return nil, storage.ErrNoSavedPages
 	}
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
