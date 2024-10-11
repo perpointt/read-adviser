644ada5a95c8db7f81891a5d06958b22900e2860
Maxim Ilyshchenko <per.pointt@gmail.com>
2024-10-11 17:19:34 +0300

    feat: add consumer.go

---
+++ consumer/event-consumer/event-consumer.go
@@ -0,0 +1,60 @@
+package event_consumer
+
+import (
+	"log"
+	"read-adviser/events"
+	"time"
+)
+
+type Consumer struct {
+	fetcher   events.Fetcher
+	processor events.Processor
+	batchSize int
+}
+
+func New(fetcher events.Fetcher, processor events.Processor, batchSize int) Consumer {
+	return Consumer{
+		fetcher:   fetcher,
+		processor: processor,
+		batchSize: batchSize,
+	}
+}
+
+func (c Consumer) Start() {
+	for {
+		gotEvents, err := c.fetcher.Fetch(c.batchSize)
+		if err != nil {
+			log.Printf("[ERR] consumer: %s", err.Error())
+			continue
+		}
+
+		if len(gotEvents) == 0 {
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		if err := c.handleEvents(gotEvents); err != nil {
+			log.Print(err)
+			continue
+		}
+	}
+}
+
+/*
+1. Потеря событий: ретраи, возвращаении в хранилище, фолбек, подтверждение
+2. обработка всей пачки: останавливаеться после первой ошибки, счётчик ошибок
+3. паралельная обработка
+*/
+
+func (c Consumer) handleEvents(events []events.Event) error {
+	for _, event := range events {
+		log.Printf("got new event: %s", event.Text)
+
+		if err := c.processor.Process(event); err != nil {
+			log.Printf("can't handle event: %s", err.Error())
+			continue
+		}
+	}
+
+	return nil
+}
